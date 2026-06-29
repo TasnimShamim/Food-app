d77f89b5c0d9c9317cc8530549138abb8e414c44
TasnimShamim <tasnimshamim35@gmail.com>
2026-06-29 22:34:21 +0530

    made backend

--- src/app.js
+++ src/app.js
@@ -1,14 +1,25 @@
 // create server
-import express from "express";
-import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.routes.js";
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const authRoutes = require('./routes/auth.routes');
+const foodRoutes = require('./routes/food.routes');
+const foodPartnerRoutes = require('./routes/food-partner.routes');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 app.use(cookieParser());
 app.use(express.json());
- app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-app.use("/api", authRoutes);
 
-export default app;
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.use('/api/auth', authRoutes);
+app.use('/api/food', foodRoutes);
+app.use('/api/food-partner', foodPartnerRoutes);
+
+module.exports = app;
